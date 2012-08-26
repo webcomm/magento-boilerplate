60eb5461fb3187a6c251f7eea93da1a4ea094712
Ben Corlett <bencorlett@me.com>
2012-08-27 09:37:06 +1000

    Utilising awesome placeholder fallbacks using Modernizr.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- skin/frontend/mytheme/default/js/script.js
+++ skin/frontend/mytheme/default/js/script.js
@@ -1,3 +1,47 @@
 jQuery(document).ready(function($) {
 	
+	// Check placeholder browser support
+	if ( ! Modernizr.input.placeholder)
+	{
+	
+		// Set placeholder values
+		$(this).find('[placeholder]').each(function()
+		{
+			// Ff field is empty
+			if ($(this).val() == '')
+			{
+				$(this).val( $(this).attr('placeholder') );
+			}
+		});
+
+		// Focus and blur of placeholders
+		$('[placeholder]').focus(function()
+		{
+			if ($(this).val() == $(this).attr('placeholder'))
+			{
+				$(this).val('');
+				$(this).removeClass('placeholder');
+			}
+		}).blur(function()
+		{
+			if ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))
+			{
+				$(this).val($(this).attr('placeholder'));
+				$(this).addClass('placeholder');
+			}
+		});
+
+		// Remove placeholders on submit
+		$('[placeholder]').closest('form').submit(function()
+		{
+			$(this).find('[placeholder]').each(function()
+			{
+				if ($(this).val() == $(this).attr('placeholder'))
+				{
+					$(this).val('');
+				}
+			})
+		});
+		
+	}
 });
