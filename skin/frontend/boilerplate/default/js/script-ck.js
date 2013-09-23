5bb773d0172009e77e1462a59a287a75d67beda7
Ben Corlett <bencorlett@me.com>
2013-09-23 13:10:55 +1000

    Work on fixing responsive top menu (by hiding children for now), also SEO site map.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- skin/frontend/boilerplate/default/js/script-ck.js
+++ skin/frontend/boilerplate/default/js/script-ck.js
@@ -9329,68 +9329,6 @@ if ( typeof window === "object" && typeof window.document === "object" ) {
 
 
 /* **********************************************
-     Begin script.js
-********************************************** */
-
-jQuery.noConflict();
-;(function($) {
-    'use strict';
-
-    function Site(settings) {
-
-        this.windowLoaded = false;
-
-    }
-
-    Site.prototype = {
-        constructor: Site
-
-        , start: function() {
-            var me = this;
-
-            $(window).load(function() {
-                me.windowLoaded = true;
-            });
-
-            this.attach();
-        }
-
-        , attach: function() {
-            this.attachMedia();
-        }
-
-        , attachMedia: function() {
-            var $links = $('[data-toggle="media"]');
-            if ( ! $links.length) return;
-
-            // When somebody clicks on a link, slide the
-            // carousel to the slide which matches the
-            // image index and show the modal
-            $links.on('click', function(e) {
-                e.preventDefault();
-
-                var $link = $(this),
-                   $modal = $($link.attr('href')),
-                $carousel = $modal.find('.carousel'),
-                    index = parseInt($link.data('index'));
-
-                $carousel.carousel(index);
-                $modal.modal('show');
-
-                return false;
-            });
-        }
-    }
-
-    jQuery(document).ready(function($) {
-        var site = new Site();
-        site.start();
-    });
-
-})(jQuery);
-
-
-/* **********************************************
      Begin carousel.js
 ********************************************** */
 
@@ -9611,3 +9549,65 @@ jQuery.noConflict();
   })
 
 }(window.jQuery);
+
+
+/* **********************************************
+     Begin script.js
+********************************************** */
+
+jQuery.noConflict();
+;(function($) {
+    'use strict';
+
+    function Site(settings) {
+
+        this.windowLoaded = false;
+
+    }
+
+    Site.prototype = {
+        constructor: Site
+
+        , start: function() {
+            var me = this;
+
+            $(window).load(function() {
+                me.windowLoaded = true;
+            });
+
+            this.attach();
+        }
+
+        , attach: function() {
+            this.attachMedia();
+        }
+
+        , attachMedia: function() {
+            var $links = $('[data-toggle="media"]');
+            if ( ! $links.length) return;
+
+            // When somebody clicks on a link, slide the
+            // carousel to the slide which matches the
+            // image index and show the modal
+            $links.on('click', function(e) {
+                e.preventDefault();
+
+                var $link = $(this),
+                   $modal = $($link.attr('href')),
+                $carousel = $modal.find('.carousel'),
+                    index = parseInt($link.data('index'));
+
+                $carousel.carousel(index);
+                $modal.modal('show');
+
+                return false;
+            });
+        }
+    }
+
+    jQuery(document).ready(function($) {
+        var site = new Site();
+        site.start();
+    });
+
+})(jQuery);
