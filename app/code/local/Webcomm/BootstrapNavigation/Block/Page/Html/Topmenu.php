f036a8cc3e2aa6dfd53626db794ca3cc495a4527
Ben Corlett <bencorlett@me.com>
2014-07-13 13:16:07 +1000

    Refactor menu override.
    Regular expressions are too problematic and hard to maintain. Fixes #24.

--- app/code/local/Webcomm/BootstrapNavigation/Block/Page/Html/Topmenu.php
+++ app/code/local/Webcomm/BootstrapNavigation/Block/Page/Html/Topmenu.php
@@ -5,17 +5,72 @@ class Webcomm_BootstrapNavigation_Block_Page_Html_Topmenu extends Mage_Page_Bloc
     /**
      * {@inheritDoc}
      */
-    public function getHtml($outermostClass = '', $childrenWrapClass = '')
+    protected function _getHtml(Varien_Data_Tree_Node $menuTree, $childrenWrapClass)
     {
-        $html = parent::getHtml($outermostClass, $childrenWrapClass);
+        $html = '';
 
-        if (Mage::getStoreConfig('catalog/navigation/top_in_dropdown')) {
-            $html = $this->_addDropdownLink($html);
+        $children = $menuTree->getChildren();
+        $parentLevel = $menuTree->getLevel();
+        $childLevel = is_null($parentLevel) ? 0 : $parentLevel + 1;
+
+        $counter = 1;
+        $childrenCount = $children->count();
+
+        $parentPositionClass = $menuTree->getPositionClass();
+        $itemPositionClassPrefix = $parentPositionClass ? $parentPositionClass . '-' : 'nav-';
+
+        foreach ($children as $child) {
+
+            $child->setLevel($childLevel);
+            $child->setIsFirst($counter == 1);
+            $child->setIsLast($counter == $childrenCount);
+            $child->setPositionClass($itemPositionClassPrefix . $counter);
+
+            $outermostClassCode = '';
+            $outermostClass = $menuTree->getOutermostClass();
+
+            if ($childLevel == 0 && $outermostClass) {
+                $outermostClassCode = ' class="' . $outermostClass . '" ';
+                $child->setClass($outermostClass);
+            }
+            if ($child->hasChildren()) {
+                $outermostClassCode .= ' data-toggle="dropdown" ';
+            }
+
+            $html .= '<li ' . $this->_getRenderedMenuItemAttributes($child) . '>';
+            $html .= '<a href="' . $child->getUrl() . '" ' . $outermostClassCode . '><span>';
+            $html .= $this->escapeHtml($child->getName());
+            if ($child->hasChildren()) {
+                $html .= ' <b class="caret"></b>';
+            }
+            $html .= '</span></a>';
+
+            if ($child->hasChildren()) {
+                if (!empty($childrenWrapClass)) {
+                    $html .= '<div class="' . $childrenWrapClass . '">';
+                }
+                $html .= '<ul class="level' . $childLevel . ' dropdown-menu">';
+                if ($level == 0) {
+                    $prefix = Mage::getStoreConfig('catalog/navigation/top_in_dropdown_prefix');
+                    $suffix = Mage::getStoreConfig('catalog/navigation/top_in_dropdown_suffix');
+                    $html .= '<li class="level1 level-top-in-dropdown">';
+                    $html .= '<a href="'.$child->getUrl().'"><span>';
+                    $html .= $this->escapeHtml($prefix.' '.$child->getName().' '.$suffix);
+                    $html .= '</span></a>';
+                    $html .= '</li>';
+                    $html .= '<li class="divider"></li>';
+                }
+                $html .= $this->_getHtml($child, $childrenWrapClass);
+                $html .= '</ul>';
+
+                if (!empty($childrenWrapClass)) {
+                    $html .= '</div>';
+                }
+            }
+            $html .= '</li>';
+
+            $counter++;
         }
-
-        $html = $this->_addToggle($html);
-        $html = $this->_addCaret($html);
-        $html = $this->_addDropdown($html);
 
         return $html;
     }
@@ -25,67 +80,12 @@ class Webcomm_BootstrapNavigation_Block_Page_Html_Topmenu extends Mage_Page_Bloc
      */
     protected function _getMenuItemClasses(Varien_Data_Tree_Node $item)
     {
-        return parent::_getMenuItemClasses($item);
-    }
+        $classes = parent::_getMenuItemClasses($item);
 
-    /**
-     * Takes a html string and appends a modified top level link inside the dropdown.
-     *
-     * @param  string  $html
-     * @return string
-     */
-    protected function _addDropdownLink($html)
-    {
-        return preg_replace_callback('/<li\s+class="level0.*?parent.*?<a\s+href="(.*?)".*?>.*?<span>(.*?)<\/span>.*?<ul\s+class="level0.*?>/', array($this, '_addDropdownLinkCallback'), $html);
-    }
+        if ($item->hasChildren()) {
+            $classes[] = 'parent';
+        }
 
-    /**
-     * Callback executed to add a modified top level link inside the dropdown. We're
-     * doing this just to ensure we're supporting PHP 5.2 as Magento does.
-     *
-     * @param  array  $matches
-     * @return string
-     */
-    protected function _addDropdownLinkCallback(array $matches)
-    {
-        $prefix = Mage::getStoreConfig('catalog/navigation/top_in_dropdown_prefix');
-        $suffix = Mage::getStoreConfig('catalog/navigation/top_in_dropdown_suffix');
-
-        $html = '<li class="level1 level-top-in-dropdown"><a href="'.$matches[1].'"><span>'.$prefix.' '.$matches[2].' '.$suffix.'</span></a>';
-        $html .= '<li class="divider"></li>';
-
-        return $matches[0].$html;
-    }
-
-    /**
-     * Adds a dropdown HTML5 attribute to top level links.
-     *
-     * @param  string  $html
-     * @return string
-     */
-    protected function _addToggle($html)
-    {
-        return preg_replace('/(<li\s+class="level0.*?parent.*?<a.*?)(>)/', '$1 data-toggle="dropdown"$2', $html);
-    }
-
-    /**
-     * Adds a caret HTML symbol to top level links.
-     *
-     * @param  string  $html
-     */
-    protected function _addCaret($html)
-    {
-        return preg_replace('/(<li\s+class="level0.*?parent.*?)(<\/a>)/', '$1 <b class="caret"></b> $2', $html);
-    }
-
-    /**
-     * Adds a CSS class to top level dropdowns.
-     *
-     * @param  string  $html
-     * @return string
-     */
-    protected function _addDropdown($html)
-    {
-        return preg_replace('/<ul\s+class="level0/', '$0 dropdown-menu', $html);
+        return $classes;
     }
 }
