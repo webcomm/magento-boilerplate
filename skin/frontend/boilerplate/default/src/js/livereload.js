472e8e59b6d6f97b7aab713c90b7d2016285f76e
Marc Addeo <marcaddeo@gmail.com>
2014-09-16 14:24:49 -0400

    Update livereload.js to remove protocol
    When you force HTTPS on the front end this makes the browser throw a warning about insecure content. Removing the protocol fixes this problem

--- skin/frontend/boilerplate/default/src/js/livereload.js
+++ skin/frontend/boilerplate/default/src/js/livereload.js
@@ -22,4 +22,4 @@
  * THE SOFTWARE.
  */
 
-document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
+document.write('<script src="//' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
